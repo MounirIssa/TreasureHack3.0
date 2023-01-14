2a3385cd3f4588d75f188042415773ec55f1608e
MounirIssa <122484250+MounirIssa@users.noreply.github.com>
2023-01-13 17:21:33 -0800

    Update practicingdistilibert.py

--- practicingdistilibert.py
+++ practicingdistilibert.py
@@ -1,9 +1,6 @@
 from transformers import pipeline
 import torch
 
-import torch
-from transformers import pipeline
-
 speech_recognizer = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
 
 print(classifier("I am going to smack you."))
